a0c48ffa2c178bc4906fd2694ae038e58b16e772
sabiransari1 <ansariisabir1@gmail.com>
2023-10-30 20:39:40 +0530

    updates

--- js/doctordashboard.js
+++ js/doctordashboard.js
@@ -14,8 +14,90 @@ const getappointments = async () => {
       },
     });
 
-    console.log(res);
+    let data = await res.json();
+
+    getData(data);
   } catch (error) {
     console.log(error);
   }
+};
+
+const getData = (data) => {
+  cont.innerHTML = null;
+
+  data?.forEach((item) => {
+    cont.append(
+      getCard(
+        item._id,
+        item.image,
+        item.name,
+        item.specialization,
+        item.experience,
+        item.location,
+        item.date,
+        item.slots,
+        item.fee
+      )
+    );
+  });
+};
+
+const getCard = (
+  id,
+  image,
+  name,
+  specialization,
+  experience,
+  location,
+  date,
+  slots,
+  fee
+) => {
+  let div = document.createElement('div');
+  div.setAttribute('class', 'divx');
+
+  let img = document.createElement('img');
+  img.setAttribute('src', image);
+
+  let namex = document.createElement('h3');
+  namex.innerText = name;
+
+  let specializationx = document.createElement('p');
+  specializationx.innerText = specialization;
+
+  let experiencex = document.createElement('p');
+  experiencex.innerText = experience;
+
+  let locationx = document.createElement('p');
+  locationx.innerText = location;
+
+  let datex = document.createElement('p');
+  datex.innerText = date;
+
+  let slotsx = document.createElement('p');
+  slotsx.innerText = slots;
+
+  let feex = document.createElement('p');
+  feex.innerText = fee;
+
+  let editx = document.createElement('button');
+  editx.innerText = 'Edit';
+
+  let deletex = document.createElement('button');
+  deletex.innerText = 'Delete';
+
+  div.append(
+    img,
+    namex,
+    specializationx,
+    experiencex,
+    locationx,
+    datex,
+    slotsx,
+    feex,
+    editx,
+    deletex
+  );
+
+  return div;
 };
